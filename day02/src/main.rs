1b09521a93a2d6c4b1eccbcdef5e42ed28588bcc
Dominik Berger <dominik.berger@protonmail.com>
2021-12-02 10:23:01 +0100

    add day02 part 1

---
+++ day02/src/main.rs
@@ -0,0 +1,75 @@
+use std::str::FromStr;
+use std::string::ParseError;
+use inputs;
+use crate::Direction::{DOWN, FORWARD, UP};
+
+#[derive(Debug)]
+enum Direction {
+    FORWARD,
+    DOWN,
+    UP
+}
+
+impl FromStr for Direction {
+    type Err = ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s {
+            "forward" => FORWARD,
+            "down" => DOWN,
+            "up" => UP,
+            _ => panic!()
+        })
+    }
+}
+
+#[derive(Debug)]
+struct Movement {
+    direction: Direction,
+    steps: u16
+}
+
+impl FromStr for Movement {
+    type Err = ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let ss: Vec<&str> = s.split(' ').collect();
+
+        let direction = Direction::from_str(ss[0])?;
+        let steps = ss[1].parse::<u16>().unwrap();
+
+        Ok(Movement { direction ,steps })
+    }
+}
+
+#[derive(Debug)]
+struct Position {
+    x: i32,
+    y: i32
+}
+
+fn solve_part1(ms: Vec<Movement>) -> Position {
+    let mut pos = Position { x: 0, y: 0 };
+
+    for m in ms {
+        match m.direction {
+            FORWARD => pos.x += (m.steps as i32),
+            DOWN => pos.y += (m.steps as i32),
+            UP => pos.y -= (m.steps as i32)
+        }
+    }
+
+    pos
+}
+
+fn main() {
+    let inputs: Vec<Movement> = inputs::get_input_split(2).iter()
+        .map(|i| Movement::from_str(i).unwrap())
+        .collect();
+
+    println!("{:?}", inputs);
+    let position = solve_part1(inputs);
+    println!("{:?}", position);
+
+    println!("{}", (position.x * position.y));
+}
