4cd44f5130238f246c94f29822e84d51c8bb0882
Dominik Berger <dominik.berger@protonmail.com>
2021-12-05 12:35:30 +0100

    add template

---
+++ template/src/main.rs
@@ -0,0 +1,19 @@
+use inputs;
+
+
+fn part1(inputs: Vec<String>) {
+
+}
+
+fn part2(inputs: Vec<String>) {
+
+}
+
+fn main() {
+    let inputs: Vec<String> = inputs::get_input_split(3);
+
+
+    println!("{:?}", inputs);
+
+    part1(inputs);
+}
