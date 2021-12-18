8ea92f9356b39c056f95cc3ade35663e8207681d
Dominik Berger <dominik.berger@protonmail.com>
2021-12-18 16:24:43 +0100

    add day17 part 1

---
+++ src/bin/day17.rs
@@ -0,0 +1,66 @@
+use std::borrow::BorrowMut;
+use std::collections::{HashMap, HashSet};
+use std::fmt::{Display, Formatter};
+use std::iter::FromIterator;
+use std::ptr::write;
+use std::str::{Chars, FromStr};
+use std::string::ParseError;
+use std::thread::current;
+
+use itertools::{fold, Itertools};
+use phf::phf_map;
+use tailcall::tailcall;
+
+const E1: &str = "target area: x=20..30, y=-10..-5";
+const E2: &str = "";
+const E3: &str = "";
+const E4: &str = "";
+const INPUT: &str = "target area: x=248..285, y=-85..-56";
+
+fn part1() {
+    let (_, _, mut xs, mut ys): (&str, &str, &str, &str) = INPUT.split(" ").collect_tuple().unwrap();
+    xs = xs.split("=").last().unwrap().split(",").nth(0).unwrap();
+    ys = ys.split("=").last().unwrap();
+
+    let (from_x, to_x): (i32, i32) = xs.split("..").map(|n| n.parse().unwrap()).collect_tuple().unwrap();
+    let (from_y, to_y): (i32, i32) = ys.split("..").map(|n| n.parse().unwrap()).collect_tuple().unwrap();
+
+    let mut max_y_found = 0;
+
+    for x_acceleration in 0..5_000 {
+        println!("x acc = {}", x_acceleration);
+        for y_acceleration in 0..5_000 {
+            if let Some(max_y) = try_jump(x_acceleration, y_acceleration, (from_x, to_x), (from_y, to_y)) {
+                if max_y > max_y_found { max_y_found = max_y; }
+            }
+        }
+    }
+
+    println!("{}", max_y_found);
+}
+
+fn try_jump(mut x_acc: i32, mut y_acc: i32, (from_x, to_x): (i32, i32), (from_y, to_y): (i32, i32)) -> Option<i32> {
+    let mut max_y = 0;
+
+    let (mut x, mut y) = (0, 0);
+    loop {
+        // println!("{:?}", (x, y));
+        if y > max_y { max_y = y; }
+        if x > to_x || y < from_y { return None; }
+        if x >= from_x && x <= to_x && y >= from_y && y <= to_y { break; }
+
+        x += x_acc;
+        y += y_acc;
+
+        if x_acc != 0 { x_acc -= 1; }
+        y_acc -= 1;
+    }
+
+    Some(max_y)
+}
+
+fn part2() {}
+
+fn main() {
+    part1();
+}
